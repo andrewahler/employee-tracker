f6fbf39a72a5cf373a37eed6f3a7ac01c4c4d4e7
Andrew Ahler <andrewahler@gmail.com>
2021-03-11 07:34:07 -0600

    added schema

--- db/seed.sql
+++ db/seed.sql
@@ -1,11 +1,11 @@
-use employees;
+use employeetracker;
 
 INSERT INTO department
     (name)
 VALUES
     ('Sales'),
     ('Human Resources'),
-    ('Engineering'),
+    ('Production'),
     
 INSERT INTO role
     (title, salary, department_id)
@@ -14,18 +14,18 @@ VALUES
     ('Salesperson', 22000, 1),
     ('HR Manager', 100000, 2),
     ('HR', 65000, 2),
-    ('Development Manager', 100000, 3),
-    ('Software Engineer', 750000, 3),
+    ('Production Manager', 100000, 3),
+    ('Software developer', 750000, 3),
  
  
-INSERT INTO employee
+INSERT INTO employeetracker
     (first_name, last_name, role_id, manager_id)
 VALUES
     ('John', 'Mcclain', 1, NULL),
-    ('Rocky', 'Balboa', 2, 1),
-    ('Ricky', 'Bobby', 3, NULL),
-    ('Delores', 'Clayborn', 4, 3),
-    ('Conald', 'Peterson', 5, NULL),
-    ('Tony', 'Johns', 6, 5),
     ('Tom', 'Segura', 7, NULL),
+    ('Ricky', 'Bobby', 3, NULL),
+    ('Conald', 'Peterson', 5, NULL),
+    ('Rocky', 'Balboa', 2, 1),
+    ('Delores', 'Clayborn', 4, 3),
+    ('Tony', 'Johns', 6, 5),
     ('Christina', 'Pazsitzky', 8, 7);
